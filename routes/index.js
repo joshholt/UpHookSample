99c51ee2a86ab5f01e36c46021be11f56a5c9e77
Josh Holt <holt.josh@gmail.com>
2012-05-08 21:08:04 -0400

    Testing up-hook again this time with a commit not from the GH UI

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@
   module.exports = {
     index: function(req, res, next) {
       return res.render('index', {
-        title: 'Holts.no-ip.org'
+        title: 'holts.no-ip.org'
       });
     }
   };
